687c5dd51a80bb69e1fe768b0c393cf21599de73
Pablo <psanch2103@gmail.com>
2019-10-14 00:29:22 +0200

    Some changes to ease installation

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+
+install_required = list()
+
+with open('environments/requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='probvis',
     version='1.0.0',
     #packages=['probvis', 'probvis.distributions', 'probvis.graphical_models'],
-    packages=find_packages(exclude=['images', 'tests']),
+    packages=find_packages(exclude=['images', 'tests', 'environments']),
+    install_requires=required,
     url='https://github.com/psanch21/prob-visualize',
     license='MIT License',
     author='Pablo Sanchez',
